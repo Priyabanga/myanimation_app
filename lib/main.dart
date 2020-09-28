2d870710e5c3005bfd4689d70b993d9dbe5d5940
Priyabanga <priya.banga_ccv17@gla.ac.in>
2020-09-28 10:18:55 +0530

    animation added

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:myanimation_app/myanim.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Animation'),
+        ),
+        body: MyA1(),
+      ),
+    ),
+  );
+}
